d9ff1354b31d676551f8de269b33fe15175fbf78
bennettmackenzie <bamsubscriptions@gmail.com>
2022-06-02 21:48:09 +1000

    lil closer

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,13 +6,14 @@ class GamesController < ApplicationController
   def new
     vowels = Array.new(3) { %w[A E I O U].sample }
     letters = Array.new(6) { %w[B C D F G H J K L M N P Q R S T V W X Y Z].sample }
-    @letters = vowels.concat(letters).shuffle.to_a
+    @letters = vowels.concat(letters).shuffle
   end
 
   def score
     @word = params[:word]
+    @letters = params[:theletters]
     @word_letters = @word.split('')
-    @check_guess = @word_letters.all? { |letter| @word_letters.count(letter) <= @letters.count(letter) }
+    raise
 
     # incorrect = "Sorry, but #{@word} can't be made out of #{@letters}"
     # invalid = "Sorry, but #{@word} does not seem to be a valid English word..."
